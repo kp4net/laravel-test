86fb17527c8017b34b199ce44fa567452eb60fab
Krunal Palaniya <krunal@buddyboss.com>
2022-06-18 18:28:43 +0530

    Added database seeds changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, LaratrustUserTrait;
 
     /**
      * The attributes that are mass assignable.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\BusinessService;
 use App\Models\MenuItem;
 use App\Models\Workshop;
 use App\Models\Event;
+use App\Models\Leave;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\RoleUser;
+use App\Models\BookingTime;
 
 class DatabaseSeeder extends Seeder
 {
@@ -114,11 +120,134 @@ class DatabaseSeeder extends Seeder
         ]);
     }
 
+    protected function seedRoles() {
+        $roles = [
+            [
+                'name' => 'superadmin',
+                'display_name' => 'Super Admin',
+                'description' => 'Super Admin',
+            ],
+            [
+                'name' => 'employee',
+                'display_name' => 'Employee',
+                'description' => 'Employee',
+            ],
+            [
+                'name' => 'customer',
+                'display_name' => 'Customer',
+                'description' => 'Customer',
+            ]
+        ];
+
+        User::truncate();
+        RoleUser::truncate();
+        Role::truncate();
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
+    }
+
+    protected function seedUsers() {
+        User::truncate();
+        $user = new User();
+        $user->name = 'Super Admin';
+        $user->email = 'admin@example.com';
+        $user->email_verified_at = date('Y-m-d H:i:s');
+        $user->password = '123456';
+
+        $user->save();
+
+        $user->attachRole(Role::select('id', 'name')->where('name', 'superadmin')->first()->id);
+
+        $emp = new User();
+        $emp->name = 'Employee';
+        $emp->email = 'employee@example.com';
+        $emp->email_verified_at = date('Y-m-d H:i:s');
+        $emp->password = '123456';
+
+        $emp->save();
+
+        $emp->attachRole(Role::select('id', 'name')->where('name', 'employee')->first()->id);
+    }
+
+    protected function seedServices() {
+        BusinessService::truncate();
+        BusinessService::insert([
+            [
+                'name' => 'Men Haircut',
+                'slug' => 'men-hair-cut',
+                'description' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit. Velit scelerisque in dictum non consectetur a erat nam at. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Mauris pellentesque pulvinar pellentesque habitant morbi. Auctor elit sed vulputate mi sit amet mauris. Id nibh tortor id.</p>
+                <br><p>Eu augue ut lectus arcu bibendum at vaes mi tempus imperdiet nulla malesuada pellentesque elit. Pellentesque id nibh tortor id aliquet lectus proin. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Volutpat blandit aliquam etiam erat velit. Tempus egestas sed sed risus pretium quam vulputate dignissim.</p>',
+                'price' => '100',
+                'time' => '30',
+                'time_type' => 'minutes',
+                'discount' => '10.00',
+                'net_price' => '100.00',
+                'image' => '["hair-cut.jpeg", "hair-spa.jpg"]',
+                'default_image' => 'hair-cut.jpeg',
+                'discount_type' => 'percent',
+                'status' => 'active'
+            ],
+            [
+                'name' => 'Women Haircut',
+                'slug' => 'women-hair-cut',
+                'description' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit. Velit scelerisque in dictum non consectetur a erat nam at. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Mauris pellentesque pulvinar pellentesque habitant morbi. Auctor elit sed vulputate mi sit amet mauris. Id nibh tortor id.</p>
+                <br><p>Eu augue ut lectus arcu bibendum at vaes mi tempus imperdiet nulla malesuada pellentesque elit. Pellentesque id nibh tortor id aliquet lectus proin. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Volutpat blandit aliquam etiam erat velit. Tempus egestas sed sed risus pretium quam vulputate dignissim.</p>',
+                'price' => '150',
+                'time' => '30',
+                'time_type' => 'minutes',
+                'discount' => '15.00',
+                'net_price' => '150.00',
+                'image' => '["hair-cut.jpeg", "hair-spa.jpg"]',
+                'default_image' => 'hair-cut.jpeg',
+                'discount_type' => 'percent',
+                'status' => 'active'
+            ]
+        ]);
+    }
+
+    protected function seedLeave() {
+        Leave::truncate();
+        $leave = new Leave();
+        $leave->user_id = 2;
+        $leave->start_date = date('Y-m-d H:i:s', strtotime("+3 days"));
+        $leave->end_date = date('Y-m-d H:i:s', strtotime("+3 days"));
+        $leave->leave_type = 'public holiday';
+        $leave->status = 'approved';
+        $leave->save();
+    }
+
+    protected function seedSchedules() {
+        BookingTime::truncate();
+        $days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+        foreach($days as $day) {
+            $data[] = [
+                'user_id' => 2,
+                'day' => $day,
+                'start_time' => $day == 'saturday' ? '10:00' : '22:00',
+                'end_time' => $day == 'saturday' ? '08:00' : '20:00',
+                'multiple_booking' => 'yes',
+                'max_booking' => 3,
+                'status' => $day == 'sunday' ? 'disabled' : 'enabled',
+                'slot_duration' => 10
+            ];
+        }
+        BookingTime::insert($data);
+
+    }
+
     public function run()
     {
         DB::transaction(function($table) {
+            DB::statement("SET foreign_key_checks=0");
             $this->seedEvents();
             $this->seedMenu();
+            $this->seedRoles();
+            $this->seedUsers();
+            $this->seedServices();
+            $this->seedLeave();
+            $this->seedSchedules();
+            DB::statement("SET foreign_key_checks=1");
         });
     }
 }
